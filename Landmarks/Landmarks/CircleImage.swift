d7bbd48499a83d6d460934a0c37303fe08e3fc9c
Myat Thu Ko <nymyatthuko@gmail.com>
2020-11-27 17:20:15 -0500

    Finished Creating and Combining Views

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Myat Thu Ko on 11/27/20.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
