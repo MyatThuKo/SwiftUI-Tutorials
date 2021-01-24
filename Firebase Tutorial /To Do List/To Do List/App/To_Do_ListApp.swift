aee665db10e3d3bec4ca66c3df863363697ec3c8
Myat Thu Ko <nymyatthuko@gmail.com>
2021-01-23 23:36:14 -0500

    Firebase Set Up

--- Firebase Tutorial /To Do List/To Do List/App/To_Do_ListApp.swift
+++ Firebase Tutorial /To Do List/To Do List/App/To_Do_ListApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct To_Do_ListApp: App {
@@ -13,5 +14,15 @@ struct To_Do_ListApp: App {
         WindowGroup {
             TaskListView()
         }
+    }
+}
+
+// AppDeleagate.swift
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
     }
 }
