d17c069837f8168feb960494a4cf0124043ab572
Myat Thu Ko <nymyatthuko@gmail.com>
2021-02-15 22:39:12 -0500

    Initial Commit for Weather App and added gitignore

---
+++ Stewart Lynch Tutorials/Weather App Tutorial/WeatherApp/WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Myat Thu Ko on 2/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stewart Lynch Tutorials/Weather App Tutorial/WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Myat Thu Ko on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
