1032957f0ac20de47bc9499f0bfcf6ce633dc4ab
Myat Thu Ko <nymyatthuko@gmail.com>
2021-02-17 22:45:25 -0500

    Building the ListView UI Completed

--- Stewart Lynch Tutorials/Weather App Tutorial/WeatherApp/WeatherApp/ContentView.swift
+++ Stewart Lynch Tutorials/Weather App Tutorial/WeatherApp/WeatherApp/ContentView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var location: String = ""
+    @State var forecast: Forecast? = nil
+    
+    let dateFormatter = DateFormatter()
+    init() {
+        dateFormatter.dateFormat = "E, MMM, d, yyyy"
+    }
     
     var body: some View {
         NavigationView {
@@ -25,8 +31,36 @@ struct ContentView: View {
                             .font(.title2)
                     })
                 }
-                
-                Spacer()
+                if let forecast = forecast {
+                    List(forecast.daily, id: \.dt) { day in
+                        VStack(alignment: .leading) {
+                            Text(dateFormatter.string(from: day.dt))
+                                .fontWeight(.bold)
+                            HStack(alignment: .top) {
+                                Image(systemName: "hourglass")
+                                    .font(.title)
+                                    .frame(width: 50, height: 50)
+                                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.green))
+                                
+                                VStack(alignment: .leading) {
+                                    Text(day.weather[0].description.capitalized)
+                                    HStack {
+                                        Text("High: \(day.temp.max, specifier: "%.0f")")
+                                        Text("Low: \(day.temp.min, specifier: "%.0f")")
+                                    }
+                                    HStack {
+                                        Text("Clouds: \(day.clouds)")
+                                        Text("POP: \(day.pop)")
+                                    }
+                                    Text("Humidity: \(day.humidity)")
+                                }
+                            }
+                        }
+                    }
+                    .listStyle(PlainListStyle())
+                } else {
+                    Spacer()
+                }
             }
             .padding(.horizontal)
             .navigationTitle("Weather App")
@@ -59,16 +93,17 @@ struct ContentView: View {
                                    dateDecodingStrategy: .secondsSince1970) { (result: Result<Forecast,APIService.APIError>) in
                     switch result {
                     case .success(let forecast):
-                        for day in forecast.daily {
-                            print(dateFormatter.string(from: day.dt))
-                            print("   Max: ", day.temp.max)
-                            print("   Min: ", day.temp.min)
-                            print("   Humidity: ", day.humidity)
-                            print("   Description: ", day.weather[0].description)
-                            print("   Clouds: ", day.clouds)
-                            print("   pop: ", day.pop)
-                            print("   IconURL: ", day.weather[0].weatherIconURL)
-                        }
+                        self.forecast = forecast
+//                        for day in forecast.daily {
+//                            print(dateFormatter.string(from: day.dt))
+//                            print("   Max: ", day.temp.max)
+//                            print("   Min: ", day.temp.min)
+//                            print("   Humidity: ", day.humidity)
+//                            print("   Description: ", day.weather[0].description)
+//                            print("   Clouds: ", day.clouds)
+//                            print("   pop: ", day.pop)
+//                            print("   IconURL: ", day.weather[0].weatherIconURL)
+//                        }
                     case .failure(let apiError):
                         switch apiError {
                         case .error(let errorString):
