99a7b7d770c252470a8f662efbd161dd880f2485
Myat Thu Ko <nymyatthuko@gmail.com>
2021-02-18 19:10:52 -0500

    ForecastListViewModel COmpleted

---
+++ Stewart Lynch Tutorials/Weather App Tutorial/WeatherApp/WeatherApp/ForecastListViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  ForecastListViewModel.swift
+//  WeatherApp
+//
+//  Created by Myat Thu Ko on 2/18/21.
+//
+
+import CoreLocation
+import Foundation
+
+class ForecastListViewModel: ObservableObject {
+    
+    @Published var forecasts: [ForecastViewModel] = []
+    var location: String = ""
+    
+    func getWeather() {
+        
+        // Getting API Key from plist
+        guard let filePath = Bundle.main.path(forResource: "Keys", ofType: "plist") else {
+            fatalError("Couldn't find file 'Keys.plist'.")
+        }
+        let plist = NSDictionary(contentsOfFile: filePath)
+        guard let apiKey = plist?.object(forKey: "WeatherAPI") as? String else {
+            fatalError("Couldn't find key 'WeatherAPI' in 'Keys.plist'.")
+        }
+        
+        
+        let apiService = APIService.shared
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E, MMM, d"
+        CLGeocoder().geocodeAddressString(location) { (placemarks, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            if let lat = placemarks?.first?.location?.coordinate.latitude,
+               let lon = placemarks?.first?.location?.coordinate.longitude {
+                // Don't forget to use your own key
+                apiService.getJSON(urlString: "https://api.openweathermap.org/data/2.5/onecall?lat=\(lat)&lon=\(lon)&exclude=current,minutely,hourly,alerts&appid=\(apiKey)",
+                                   dateDecodingStrategy: .secondsSince1970) { (result: Result<Forecast,APIService.APIError>) in
+                    switch result {
+                    case .success(let forecast):
+                        DispatchQueue.main.async {
+                            self.forecasts = forecast.daily.map {ForecastViewModel(forecast: $0)}
+                        }
+                    case .failure(let apiError):
+                        switch apiError {
+                        case .error(let errorString):
+                            print(errorString)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
