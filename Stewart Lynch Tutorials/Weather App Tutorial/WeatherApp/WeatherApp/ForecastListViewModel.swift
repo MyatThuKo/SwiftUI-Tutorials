b1931d7259d863911f9f1e23982cfeb45ac57aeb
Myat Thu Ko <nymyatthuko@gmail.com>
2021-02-18 21:28:27 -0500

    Switching Units and AppStorage Completed

--- Stewart Lynch Tutorials/Weather App Tutorial/WeatherApp/WeatherApp/ForecastListViewModel.swift
+++ Stewart Lynch Tutorials/Weather App Tutorial/WeatherApp/WeatherApp/ForecastListViewModel.swift
@@ -7,11 +7,25 @@
 
 import CoreLocation
 import Foundation
+import SwiftUI
 
 class ForecastListViewModel: ObservableObject {
     
     @Published var forecasts: [ForecastViewModel] = []
-    var location: String = ""
+    @AppStorage("location") var location: String = ""
+    @AppStorage("system") var system: Int = 0 {
+        didSet {
+            for i in 0..<forecasts.count {
+                forecasts[i].system = system
+            }
+        }
+    }
+    
+    init() {
+        if location != "" {
+            getWeather()
+        }
+    }
     
     func getWeather() {
         
@@ -40,7 +54,7 @@ class ForecastListViewModel: ObservableObject {
                     switch result {
                     case .success(let forecast):
                         DispatchQueue.main.async {
-                            self.forecasts = forecast.daily.map {ForecastViewModel(forecast: $0)}
+                            self.forecasts = forecast.daily.map {ForecastViewModel(forecast: $0, system: self.system)}
                         }
                     case .failure(let apiError):
                         switch apiError {
