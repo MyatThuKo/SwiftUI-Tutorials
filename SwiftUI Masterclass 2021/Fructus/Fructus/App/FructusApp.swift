19449a4b4876e89616dff06a6c9e6c04b92a7a50
Myat Thu Ko <nymyatthuko@gmail.com>
2021-03-02 23:11:07 -0500

    Creating an Onboarding screen using TabView completed

---
+++ SwiftUI Masterclass 2021/Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Myat Thu Ko on 3/2/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
