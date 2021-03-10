4a070e7fd0f9f4de8dabb6945340353dcbeb51b6
Myat Thu Ko <nymyatthuko@gmail.com>
2021-03-09 22:47:18 -0500

    Completed setting up the project

---
+++ SwiftUI Masterclass 2021/Africa/Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Myat Thu Ko on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
