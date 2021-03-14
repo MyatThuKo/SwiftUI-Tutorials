7313bd54c62bddf035d964b2d767ce6b774b20c9
Myat Thu Ko <nymyatthuko@gmail.com>
2021-03-14 14:44:53 -0400

    Completed creating a JSON Decoder and making dynamic List using Swift Generics and JSON Data

---
+++ SwiftUI Masterclass 2021/Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Myat Thu Ko on 3/9/21.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String 
+}
