f7a0721d066190c71dcb21c4f9bd8cbb9bc49305
Myat Thu Ko <nymyatthuko@gmail.com>
2021-03-14 22:53:38 -0400

    Completed MapView

---
+++ SwiftUI Masterclass 2021/Africa/Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Myat Thu Ko on 3/14/21.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    // Computed Properties
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
